391c8ef5ff0f6ec3618a6daf412ffff230110e8d
@hexlet/cli <support@hexlet.io>
2024-03-27 12:01:36 +0300

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -5,10 +5,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,7 +36,7 @@ public class PostsController {
         newPost.setTitle(post.getTitle());
         newPost.setBody(post.getBody());
         newPost.setSlug(post.getSlug());
-        posts.add(post);
+        posts.add(newPost);
         return ResponseEntity.status(HttpStatus.CREATED).body(post);
     }
 }
