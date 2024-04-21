4060321fb69e0064a391b0585d43c6581ea1f62d
@hexlet/cli <support@hexlet.io>
2024-04-21 17:09:24 +0300

    submit java-spring-ru/search

--- java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
+++ java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -19,23 +19,33 @@ public class ProductSpecification {
     }
 
     private Specification<Product> withCategoryId(Long categoryId) {
-        return (root, query, cb) -> categoryId == null ? cb.conjunction() : cb.equal(root.get("category").get("id"), categoryId);
+        return (root, query, cb) -> categoryId == null
+                ? cb.conjunction()
+                : cb.equal(root.get("category").get("id"), categoryId);
     }
 
     private Specification<Product> withTitleCont(String titleCont) {
-        return (root, query, cb) -> titleCont == null ? cb.conjunction() : cb.equal(root.get("titleCont"), titleCont);
+        return (root, query, cb) -> titleCont == null
+                ? cb.conjunction()
+                : cb.equal(root.get("titleCont"), titleCont);
     }
 
     private Specification<Product> withPriceLt(Integer price) {
-        return (root, query, cb) -> price == null ? cb.conjunction() : cb.lessThan(root.get("price"), price);
+        return (root, query, cb) -> price == null
+                ? cb.conjunction()
+                : cb.lessThan(root.get("price"), price);
     }
 
     private Specification<Product> withPriceGt(Integer price) {
-        return (root, query, cb) -> price == null ? cb.conjunction() : cb.greaterThan(root.get("price"), price);
+        return (root, query, cb) -> price == null
+                ? cb.conjunction()
+                : cb.greaterThan(root.get("price"), price);
     }
 
     private Specification<Product> withRatingGt(Double rating) {
-        return (root, query, cb) -> rating == null ? cb.conjunction() : cb.greaterThan(root.get("rating"), rating);
+        return (root, query, cb) -> rating == null
+                ? cb.conjunction()
+                : cb.greaterThan(root.get("rating"), rating);
     }
 }
 // END
